6e68b71998b6420969fbd1362fb39b8fd1a92c5b
Mike Andrade <mike.andrade.oliveira@gmail.com>
2022-12-23 16:02:53 -0300

    Updating grade

--- src/main/java/com/ltp/gradesubmission/controller/GradeController.java
+++ src/main/java/com/ltp/gradesubmission/controller/GradeController.java
@@ -5,6 +5,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -15,14 +16,19 @@ public class GradeController {
     List<Grade> studentsGrades = new ArrayList<>();
 
     @GetMapping("/")
-    public String getForm(Model model){
-        model.addAttribute("grade", new Grade());
+    public String getForm(Model model, @RequestParam(required = false) String name){
+        model.addAttribute("grade", getGradeIndex(name) == -1000 ? new Grade() : studentsGrades.get(getGradeIndex(name)));
         return "form";
     }
 
     @PostMapping("/handleSubmit")
     public String submitForm(Grade grade){
-        studentsGrades.add(grade);
+        int index = getGradeIndex(grade.getName());
+        if(index == -1000){
+            studentsGrades.add(grade);
+        } else {
+            studentsGrades.set(index, grade);
+        }
         return "redirect:/grades";
     }
 
@@ -30,5 +36,14 @@ public class GradeController {
     public String getGrades(Model model){
         model.addAttribute("grades", studentsGrades);
         return "grades";
+    }
+
+    public Integer getGradeIndex(String name){
+        for (int i = 0; i < studentsGrades.size(); i++) {
+            if(studentsGrades.get(i).getName().equals(name)){
+                return i;
+            }
+        }
+        return -1000;
     }
 }
